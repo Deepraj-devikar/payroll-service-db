5a4b125db64554cf413edfeb489d2e2401927386
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-11-21 14:32:06 +0530

    ADDED: create table employee payroll service with id, employee name, salary, start date

--- PayrollService.sql
+++ PayrollService.sql
@@ -9,3 +9,11 @@ SHOW DATABASES;
 
 -- database change to use payroll_service database
 USE payroll_service;
+
+-- creating table employee payroll with id, employee name, salary, start date
+CREATE TABLE employee_payroll (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    employee_name VARCHAR(30),
+    salary DECIMAL(20, 10),
+    start_date DATE
+);
