bb58bd0200931fa10847cd5953bc2a9e097a53b0
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-11-21 14:06:16 +0530

    ADDED: creating payroll service database and use it

---
+++ PayrollService.sql
@@ -0,0 +1,11 @@
+-- show all databases
+SHOW DATABASES;
+
+-- creating payroll_service database
+CREATE DATABASE payroll_service;
+
+-- will show all databases including payroll_service
+SHOW DATABASES;
+
+-- database change to use payroll_service database
+USE payroll_service;
