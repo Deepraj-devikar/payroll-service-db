37c57c6ad5fd5e7b36a3698c9ba295c8e18143c0
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-11-21 14:50:55 +0530

    ADDED: insert data to employee payroll table

--- PayrollService.sql
+++ PayrollService.sql
@@ -17,3 +17,18 @@ CREATE TABLE employee_payroll (
     salary DECIMAL(20, 10),
     start_date DATE
 );
+
+-- inserting data to employee payroll table
+INSERT INTO employee_payroll (employee_name, salary, start_date) 
+VALUES ('Pushpak', 20000, '2018-02-19');
+
+INSERT INTO employee_payroll 
+VALUES (2, 'Jayesh', 50000.54, '2019-02-19');
+
+INSERT INTO employee_payroll 
+VALUES (3, 'Rupesh', 60500.54, '2019-05-19'), 
+(4, 'Ram', 999999.999, '2020-06-25');
+
+INSERT INTO employee_payroll (employee_name, salary, start_date) 
+VALUES ('Narang', 555555.56, '2021-10-21'), 
+('Sarang', 6666666.78, '2020-08-08');
